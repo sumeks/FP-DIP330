af17985e5d7ee9783d9c944d088078464070853c
sumeks <hawkqwerty1@gmail.com>
2018-11-04 07:00:43 +0200

    FP5

--- FP5/src/Isogram/Isogram.kt
+++ FP5/src/Isogram/Isogram.kt
@@ -12,14 +12,4 @@ object Isogram{
             .eachCount()
             .any {it.value > 1}
 
-
-
-
-//            if(input.length>1){
-//                input.toLowerCase().any {!input.substring(1, input.length).contains(it) && isIsogram(input.substring(1, input.length).replace("-","")) }}
-//             else
-//                true
-
 }
-
-
